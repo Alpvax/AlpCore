d166ef7e125d31342e8e451f324414e2f5619710
Alpvax <alpvax@netscape.net>
2015-03-26 17:03:00 +0000

    Updated latestForge.py to handle new downloads page

--- latestForge.py
+++ latestForge.py
@@ -2,99 +2,90 @@
 import urllib2, re, HTMLParser, os
       
 class ForgeParser(HTMLParser.HTMLParser):
-    def __init__(self, reference=None, mc_version=None, table_id="promotions_table"):
+    def __init__(self):
         HTMLParser.HTMLParser.__init__(self)
-        ref = ""
-        if mc_version:
-            ver = str(mc_version).split(".")
-            for i in range(len(ver)):
-                if not ver[i]:
-                    ver[i] = '\d'
-            ref = ".".join(ver) + "-"
-        if reference:
-            ref += reference
-        self.refpattern = re.compile(ref)
-        if ref:
-            print("Finding versions matching: \"%s\"" % self.refpattern.pattern)
-        else:
-            print("Finding all versions")
-        self.table = table_id
-        self.state = 0#0 if not started, 1 if in progress, 2 if finished
-        self.recording = False
-        self.currentTag = ""
-        self.currentRef = ""
-        self.currentCol = 0
-        self.link = ""
-        self.linkpattern = re.compile("http://adf.ly/\d+/")
-        self.data = {}
+        self.vpattern = re.compile("\d+\.\d+\.\d+\.\d+")
+        self.linkpattern = re.compile("\w+://[\w\./\-]*(?!.*://)")
+        self.recording = 0
+        self. recorded = 0
+        self.rv = 0
+        self.data = {"Links":{}}
 
     def handle_starttag(self, tag, attributes):
-        if self.state == 2:
+        if self.recorded:
             return
-        self.currentTag = tag
-        if self.recording:
-            for attr in attributes:
-                if tag == 'table':
-                    if attr[0] == 'id' and attr[1] == self.table:
-                        self.state = 1
-                elif tag == 'a':
-                    if attr[0] == 'href':
-                        self.link = attr[1]
+        if tag == 'span':
+            self.rv = 1
+        if self.rv and tag == 'strong':
+            self.rv = 2
+        
+        if tag == 'i' and "fa fa-star promo-LATEST" in getAttributes(attributes, "class"):
+            self.recording = 2
+        #if self.recording == 1 and tag == 'div' and "links" in getAttributes(attributes, "class"):
+        #    print("Links:")
+        if self.recording == 1 and tag == 'a':
+            map = getAttMap(attributes)
+            title = getAtt(map, "title")
+            link = getAtt(map, "href")
+            if title and link:
+                self.data["Links"][title[0]] = self.linkpattern.search(link[0]).group()
 
     def handle_endtag(self, tag):
-        if self.state == 2:
+        if self.recorded:
             return
-        if tag == 'table':
-            self.state = 2
-        if self.recording:
-            if tag == 'tr':
-                self.recording = False
-            if tag == 'td':
-                self.currentCol += 1
+        if self.recording and tag == 'div':
+            self.recording -= 1
+            if self.recording < 1:
+                self.recorded = 1
     
     def handle_data(self, data):
-        if self.state == 2:
+        if self.recorded:
             return
-        if self.currentTag == 'tr':
-            self.startRecording()
-        elif self.currentTag == 'a' and data == '*':
-            return
-        elif self.recording:
-            if self.currentTag == 'td':
-                if self.currentCol == 0:
-                    if self.refpattern.match(data):#data.startswith(self.ref):
-                        self.currentRef = data
-                        self.data[self.currentRef] = {'Links':{}}
-                    else:
-                        self.endRecording()
-                if self.currentCol == 1:
-                    self.data[self.currentRef]['ForgeVer'] = data
-                if self.currentCol == 2:
-                    self.data[self.currentRef]['MCVer'] = data
-                if self.currentCol == 3:
-                    self.data[self.currentRef]['Date'] = data
-            elif self.currentTag == 'a' and self.currentCol == 4:
-                self.data[self.currentRef]['Links'][data] = self.linkpattern.sub("", self.link)
-                
+        if self.rv == 2:
+            self.data["MCVer"] = data
+            self.rv = 0
+        if self.recording and self.vpattern.match(data):
+            self.data["ForgeVer"] = data
+            
     def startRecording(self):
         self.recording = True
         self.currentCol = 0
         
     def endRecording(self):
         self.recording = False
+        
+def getAttMap(attributes):
+    map = {}
+    for k, v in attributes:
+        if k in map:
+            map[k].append(v)
+        else:
+            map[k] = [v]
+    return map
+
+def getAtt(attmap, key):
+    if key in attmap:
+        return attmap[key]
+    return []
+    
+def getAttributes(attributes, key):
+    return getAtt(getAttMap(attributes), key)
+
+def getAttributesFromArr(attributes, key):
+    return [v for k, v in attributes if k == key]
 
 if __name__=="__main__":
-    p = ForgeParser("Latest")
+    p = ForgeParser()
     response = urllib2.urlopen("http://files.minecraftforge.net/")
     html = response.read()
     p.feed(html)
     p.close()
-    url = p.data["Latest"]["Links"]["Src"]
+    url = p.data["Links"]["Src"]
     fname = url.split("/")[-1]
     if os.path.exists(fname):
-        print("Latest Forge developer version \"%s\" detected, not downloading new copy" % p.data["Latest"]["ForgeVer"])
+        print("Latest Forge developer version \"%s\" detected, not downloading new copy" % p.data["ForgeVer"])
     else:
-        print("Downloading Forge developer version \"%s\" as file: \"%s\"" % (p.data["Latest"]["ForgeVer"], fname))
+        print("Downloading Forge developer version \"%s\" as file: \"%s\"" % (p.data["ForgeVer"], fname))
         zip = urllib2.urlopen(url)
         output = open(fname,'wb')
         output.write(zip.read())
