942cd944667d52b3ccb67c45b07666d463ba1acc
Alpvax <alpvax@netscape.net>
2015-04-14 18:13:12 +0100

    Added Config-related helper methods

---
+++ src/main/java/alpvax/mod/common/util/GlobalConstants.java
@@ -0,0 +1,12 @@
+package alpvax.mod.common.util;
+
+import java.io.File;
+
+/**
+ * @author Alpvax
+ *
+ */
+public class GlobalConstants
+{
+	public static File DEF_CONFIG_DIR;
+}
---
+++ src/main/java/alpvax/mod/common/util/SaveHelper.java
@@ -0,0 +1,76 @@
+package alpvax.mod.common.util;
+
+import java.io.File;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.storage.SaveHandler;
+import net.minecraftforge.common.DimensionManager;
+import net.minecraftforge.common.config.Configuration;
+
+public class SaveHelper
+{
+	public static String getWorldName()
+	{
+		SaveHandler s = getWorldSaveHandler();
+		return s != null ? s.loadWorldInfo().getWorldName() : null;
+	}
+
+	public static SaveHandler getWorldSaveHandler()
+	{
+		if(DimensionManager.getWorld(0) != null)
+		{
+			return (SaveHandler)DimensionManager.getWorld(0).getSaveHandler();
+		}
+		else if(MinecraftServer.getServer() != null)
+		{
+			MinecraftServer srv = MinecraftServer.getServer();
+			return (SaveHandler)srv.getActiveAnvilConverter().getSaveLoader(srv.getFolderName(), false);
+		}
+		return null;
+	}
+	
+	public static File getCurrentConfigDir()
+	{
+		return getCurrentConfigDir("");
+	}
+	
+	public static File getCurrentConfigDir(String subDir)
+	{
+		SaveHandler s = getWorldSaveHandler();
+		File f;
+		if(s == null)
+		{
+			f = GlobalConstants.DEF_CONFIG_DIR;
+		}
+		else
+		{
+			f = new File(s.getWorldDirectory(), "config");
+		}
+		if(subDir != null && subDir.length() > 0)
+		{
+			f = new File(f, subDir);
+		}
+		f.mkdirs();
+		return f;
+	}
+	
+	/**
+	 * @param subDir Optional subpath to look in
+	 * @param configName
+	 * @return
+	 */
+	public static Configuration[] getConfigs(String subDir, String configName)
+	{
+		File f = new File(GlobalConstants.DEF_CONFIG_DIR, subDir);
+		f.mkdirs();
+		Configuration[] res = {new Configuration(new File(f, configName)), null};
+		SaveHandler s = getWorldSaveHandler();
+		if(s != null)
+		{
+			f = new File(s.getWorldDirectory(), "config/" + subDir);
+			f.mkdirs();
+			res[1] = new Configuration(new File(f, configName));
+		}
+		return res;
+	}
+}
