468253238feedfc4926fc8a438c62426e05cec42
Alpvax <alpvax@netscape.net>
2015-04-23 17:19:06 +0100

    Added Attribute helper method

--- src/main/java/alpvax/common/util/EntityHelper.java
+++ src/main/java/alpvax/common/util/EntityHelper.java
@@ -1,6 +1,12 @@
 package alpvax.common.util;
 
+import java.security.InvalidParameterException;
+
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.IAttribute;
+import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 
@@ -22,5 +28,63 @@ public class EntityHelper
 	public static String getPlayerName(EntityPlayer player)
 	{
 		return player.getDisplayNameString();
+	}
+	
+	/**
+	 * @param overrideType 0 = override with new modifier, 1 = add values, 2 = multiply values
+	 */
+	public static void applyAttributeModifier(EntityLivingBase entity, IAttribute attribute, AttributeModifier modifier, int overrideType)
+	{
+		IAttributeInstance att = entity.getAttributeMap().getAttributeInstance(attribute);
+		AttributeModifier mod = att.getModifier(modifier.getID());
+		if(mod != null)
+		{
+			att.removeModifier(mod);
+		}
+		if(mod == null || overrideType == 0)
+		{
+			mod = modifier;
+		}
+		else
+		{
+			switch(overrideType)
+			{
+				case 1:
+					mod = new AttributeModifier(modifier.getID(), modifier.getName(), mod.getAmount() + modifier.getAmount(), modifier.getOperation());
+					break;
+				case 2:
+					mod = new AttributeModifier(modifier.getID(), modifier.getName(), mod.getAmount() * modifier.getAmount(), modifier.getOperation());
+					break;
+				default:
+					throw new InvalidParameterException("overrideType must be 0, 1 or 2! Recieved " + overrideType);
+			}
+		}
+		att.applyModifier(mod);
+	}
+
+	/**
+	 * @param overrideType 0 = remove modifier completely, 1 = subtract values, 2 = divide values
+	 */
+	public static void removeAttributeModifier(EntityLivingBase entity, IAttribute attribute, AttributeModifier modifier, int overrideType)
+	{
+		IAttributeInstance att = entity.getAttributeMap().getAttributeInstance(attribute);
+		AttributeModifier mod = att.getModifier(modifier.getID());
+		att.removeModifier(mod);
+		if(mod == null || overrideType == 0)
+		{
+			return;
+		}
+		switch(overrideType)
+		{
+			case 1:
+				mod = new AttributeModifier(modifier.getID(), modifier.getName(), mod.getAmount() - modifier.getAmount(), modifier.getOperation());
+				break;
+			case 2:
+				mod = new AttributeModifier(modifier.getID(), modifier.getName(), mod.getAmount() / modifier.getAmount(), modifier.getOperation());
+				break;
+			default:
+				throw new InvalidParameterException("overrideType must be 0, 1 or 2! Recieved " + overrideType);
+		}
+		att.applyModifier(mod);
 	}
 }
