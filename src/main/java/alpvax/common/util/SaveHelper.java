5c1459f8a1c74d2d211511b3db5e0f924c2df407
Alpvax <Alpvax@netscape.net>
2015-05-07 22:22:27 +0100

    Redesigned save specific configs

--- src/main/java/alpvax/common/util/SaveHelper.java
+++ src/main/java/alpvax/common/util/SaveHelper.java
@@ -7,6 +7,7 @@ import net.minecraft.world.storage.ISaveFormat;
 import net.minecraft.world.storage.SaveHandler;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
 
 
 public class SaveHelper
@@ -32,7 +33,7 @@ public class SaveHelper
 		return null;
 	}
 
-	public static File getCurrentConfigDir()
+	/*public static File getCurrentConfigDir()
 	{
 		return getCurrentConfigDir("");
 	}
@@ -62,7 +63,7 @@ public class SaveHelper
 	 * @param configName
 	 * @return
 	 */
-	public static Configuration[] getConfigs(String subDir, String configName)
+	/*public static Configuration[] getConfigs(String subDir, String configName)
 	{
 		File f = new File(GlobalConstants.DEF_CONFIG_DIR, subDir);
 		f.mkdirs();
@@ -75,11 +76,122 @@ public class SaveHelper
 			res[1] = new Configuration(new File(f, configName));
 		}
 		return res;
-	}
+	}*/
 
 	public static Configuration getCurrentConfig(String subDir, String configName)
 	{
-		Configuration[] configs = SaveHelper.getConfigs(subDir, configName);
-		return configs[1] != null ? configs[1] : configs[0];
+		String path = (subDir == null || subDir.length() < 1 ? "" : subDir + "/") + configName;
+		SaveHandler s = getWorldSaveHandler();
+		if(s != null)
+		{
+			return new Configuration(new File(s.getWorldDirectory() + "/config", path));
+		}
+		return new Configuration(new File(GlobalConstants.DEF_CONFIG_DIR, path));
 	}
+
+	/**
+	 * Retrieve a property from the appropriate {@link Configuration}.<br>
+	 * Returns {@code null} if the property is not found
+	 * @param subDir The subpath (beneath /config/) the config file is located in
+	 * @param configName The name of the config file
+	 * @param category The category the key is saved under
+	 * @param key The key to retrieve
+	 */
+	public static Property getProperty(String subDir, String configName, String category, String key)
+	{
+		return getProperty(subDir, configName, category, key, null, null, null, false);
+	}
+
+	/**
+	 * Retrieve a property from the appropriate {@link Configuration}.<br>
+	 * Saves {@code defaultValue} if the property is not found
+	 * @param subDir The subpath (beneath /config/) the config file is located in
+	 * @param configName The name of the config file
+	 * @param category The category the key is saved under
+	 * @param key The key to save/retrieve
+	 * @param defaultValue The value to use if the key does not already exist
+	 * @param comment A comment for the key
+	 * @param type The {@link Property.Type} to save/retrieve
+	 */
+	public static Property getProperty(String subDir, String configName, String category, String key, String defaultValue, String comment, Property.Type type)
+	{
+		return getProperty(subDir, configName, category, key, new String[]{defaultValue}, comment, type, false);
+	}
+
+	/**
+	 * Retrieve an array property from the appropriate {@link Configuration}.<br>
+	 * Saves {@code defaultValues} if the property is not found
+	 * @param subDir The subpath (beneath /config/) the config file is located in
+	 * @param configName The name of the config file
+	 * @param category The category the key is saved under
+	 * @param key The key to save/retrieve
+	 * @param defaultValues The values to use if the key does not already exist
+	 * @param comment A comment for the key
+	 * @param type The {@link Property.Type} to save/retrieve
+	 */
+	public static Property getProperty(String subDir, String configName, String category, String key, String[] defaultValues, String comment, Property.Type type)
+	{
+		return getProperty(subDir, configName, category, key, defaultValues, comment, type, true);
+	}
+
+	private static Property getProperty(String subDir, String configName, String category, String key, String[] defaultValues, String comment, Property.Type type, boolean isList)
+	{
+		SaveHandler s = getWorldSaveHandler();
+		Configuration conf = null;
+		if(s != null)
+		{
+			conf = new Configuration(new File(s.getWorldDirectory() + "/config/" + subDir, configName));
+			conf.load();
+		}
+		if(conf == null || !conf.hasKey(category, key))
+		{
+			Configuration def = new Configuration(new File(GlobalConstants.DEF_CONFIG_DIR + "/" + subDir, configName));
+			def.load();
+			if(defaultValues == null && !def.hasKey(category, key))
+			{
+				return null;
+			}
+			Property p = isList ? def.get(category, key, defaultValues, comment, type) : def.get(category, key, defaultValues[0], comment, type);
+			def.save();
+			if(conf != null)
+			{
+				return isList ? conf.get(category, key, p.getStringList(), comment, type) : conf.get(category, key, p.getString(), comment, type);
+			}
+			return p;
+		}
+		return getPropertyAndSave(conf, category, key, defaultValues, comment, type, isList);
+	}
+
+	private static Property getPropertyAndSave(Configuration config, String category, String key, String[] defaultValues, String comment, Property.Type type, boolean isList)
+	{
+		Property p = isList ? config.get(category, key, defaultValues, comment, type) : config.get(category, key, defaultValues[0], comment, type);
+		config.save();
+		return p;
+	}
+
+	/*public static String getConfigOption(String subDir, String configName, String category, String key, String _default)
+	{
+		SaveHandler s = getWorldSaveHandler();
+		Configuration conf = null;
+		if(s != null)
+		{
+			conf = new Configuration(new File(s.getWorldDirectory() + "config/" + subDir, configName));
+			if(conf.hasKey(category, key))
+			{
+				return conf.get(category, key, _default).getString();
+			}
+		}
+		Configuration def = new Configuration(new File(GlobalConstants.DEF_CONFIG_DIR + "/" + subDir, configName));
+		Property p = null;
+		if(def.hasKey(category, key))
+		{
+			p = def.getCategory(category).get(key);
+			if(conf != null)
+			{
+				conf.getCategory(category).put(key, p);
+			}
+			return p;
+		}
+		return null;
+	}*/
 }
